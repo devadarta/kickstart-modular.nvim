a81f50882c5e962978405e0385cb967e86412880
Davila <daniel.pedrotti@gmail.com>
2025-11-02 07:40:34 -0300

    Add plugin that shows the color html/rgb/name

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -21,4 +21,12 @@ return {
       'rcarriga/nvim-notify',
     },
   },
+  {
+    'catgoose/nvim-colorizer.lua',
+    event = 'VeryLazy',
+    opts = {
+      lazy_load = true,
+      -- other setup options
+    },
+  },
 }
