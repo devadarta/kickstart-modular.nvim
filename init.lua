73e55a6179cdbc455a6e49a0d99c1ef248c88263
Daniel Pedrotti <daniel.pedrotti84@gmail.com>
2026-07-01 12:44:05 -0300

    Require custom lua files

--- init.lua
+++ init.lua
@@ -96,5 +96,8 @@ require 'pack'
 -- [[ Configure and install plugins ]]
 require 'plugins'
 
+-- [[ Custom options / keymaps / autocmds ]]
+require 'custom'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
