90a31198dcad10e66f75ac175020060c3e73e9f8
Daniel Pedrotti <daniel.pedrotti84@gmail.com>
2026-07-09 09:04:40 -0300

    Improve keys info

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -8,9 +8,9 @@ require('which-key').setup {
   icons = { mappings = vim.g.have_nerd_font },
   -- Document existing key chains
   spec = {
-    { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
-    { '<leader>t', group = '[T]oggle' },
-    { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
+    { '<leader>s', group = '[S] Search', mode = { 'n', 'v' } },
+    { '<leader>t', group = '[t] Toggle/Tabs' },
+    { '<leader>h', group = '[h] Git Hunk', mode = { 'n', 'v' } }, -- Enable gitsigns recommended keymaps first
     { 'gr', group = 'LSP Actions', mode = { 'n' } },
   },
 }
