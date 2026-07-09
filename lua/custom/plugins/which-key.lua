24ec387950c043d8e672674fdd7a5f2468f5477d
Daniel Pedrotti <daniel.pedrotti84@gmail.com>
2026-07-09 09:02:05 -0300

    Added custom keys

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -3,9 +3,19 @@ wk.setup {
   preset = 'helix',
 }
 wk.add {
+  { '<leader>r', icon = '' },
+  { '<leader>b', group = '[b] Buffers' },
+  { '<leader>w', group = '[w] Write (save) window' },
+  { '<leader>q', desc = '[q] Open diagnostic Quickfix list' },
+  { '<leader>f', desc = '[f] Format buffer' },
+  -- {
+  --   '<leader>?',
+  --   function() require('which-key').show { global = false } end,
+  --   desc = '[?] Buffer Local Keymaps (which-key)',
+  -- },
   {
-    '<leader>?',
-    function() require('which-key').show { global = false } end,
-    desc = 'Buffer Local Keymaps (which-key)',
+    '<leader>b',
+    group = '[b] Buffers',
+    expand = function() return require('which-key.extras').expand.buf() end,
   },
 }
